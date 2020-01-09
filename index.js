407de5105b0ba23f1b5a60959e2e6830d2d2cc72
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-09 20:11:21 +0900

    modify auth

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { getToken } = require('./lib/auth')
 const { sendCommand } = require('./iot-core/iot-core')
 
 const handleCommand = async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
+  if (getToken(req) !== process.env.CLOUD_FUNC_TOKEN) {
+    res.status(401).json({ message: 'Your token is invalid' })
+    return
+  }
 
   if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
@@ -10,13 +15,9 @@ const handleCommand = async (req, res) => {
     res.set('Access-Control-Max-Age', '3600')
     res.status(204).send('')
   } else if (req.get('content-type') === 'application/json') {
+    const body = req.body
     try {
-      const body = req.body
-      if (body.token !== process.env.CLOUD_FUNC_TOKEN) {
-        res.status(401).json({ message: 'Your token is invalid' })
-        return
-      }
-      await sendCommand(JSON.stringify(body.cmd))
+      await sendCommand(JSON.stringify(body))
       res.status(204).send('')
     } catch (e) {
       res.status(500).json({ message: 'failed to stringify JSON' })
