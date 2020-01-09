195d108bd55f6158403d4c5bcb2e753ba499304e
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-10 01:50:52 +0900

    fix cors

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const { getToken } = require('./lib/auth')
 const { sendCommand } = require('./iot-core/iot-core')
 
 const handleCommand = async (req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
-  if (getToken(req) !== process.env.CLOUD_FUNC_TOKEN) {
-    res.status(401).json({ message: 'Your token is invalid' })
-    return
-  }
+  res.set('Access-Control-Allow-Origin', 'http://localhost:8080')
 
   if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
     res.set('Access-Control-Allow-Methods', 'POST')
-    res.set('Access-Control-Allow-Headers', 'Content-Type')
+    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.set('Access-Control-Max-Age', '3600')
     res.status(204).send('')
+  } else if (getToken(req) !== process.env.CLOUD_FUNC_TOKEN) {
+    res.status(401).json({ message: 'Your token is invalid' })
   } else if (req.get('content-type') === 'application/json') {
     const body = req.body
     try {
-      await sendCommand(JSON.stringify(body))
+      await sendCommand(JSON.stringify(body.cmd))
       res.status(204).send('')
     } catch (e) {
       res.status(500).json({ message: 'failed to stringify JSON' })
